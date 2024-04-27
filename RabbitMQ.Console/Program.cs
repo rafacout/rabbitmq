cf8e11f152625f4674ca02cb73d962546f8034c8
Rafael Coutinho <coutinhor@knowfully.com>
2024-04-27 11:48:35 -0300

    Added rabbitmq publish message

---
+++ RabbitMQ.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, RabbitMQ!");
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+var connection = connectionFactory.CreateConnection("rabbitmq");
+
+var channel = connection.CreateModel();
+
+var person = new Person(Guid.NewGuid(), "Rafael Coutinho", DateTime.Parse("1983/07/24"));
+var personJson = JsonSerializer.Serialize(person);
+var byteArray = Encoding.UTF8.GetBytes(personJson);
+
+channel.BasicPublish("curso-rabbitmq", "hr.person-created", null, byteArray);
+
+Console.WriteLine($"Message published: {personJson}");
+Console.ReadLine();
+
+
+public class Person
+{
+    public Person(Guid id, string name, DateTime birthDate)
+    {
+        Id = id;
+        Name = name;
+        BirthDate = birthDate;
+    }
+    
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+}
